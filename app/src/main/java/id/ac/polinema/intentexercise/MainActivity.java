d5591b603be3b123f46399d9f2a4c7c3c0b00576
nabnab35 <nabilaem353@gmail.com>
2022-10-05 20:31:54 +0700

    updated

--- app/src/main/java/id/ac/polinema/intentexercise/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/MainActivity.java
@@ -5,17 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+	private Button btn_regist;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		btn_regist = findViewById(R.id.btn_register);
 
-	public void handleRegister(View view) {
-		Intent intent = new Intent(this, RegisterActivity.class);
-		startActivity(intent);
+		btn_regist.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent pindah = new Intent(MainActivity.this,RegisterActivity.class);
+				startActivity(pindah);
+			}
+		});
 	}
 }
